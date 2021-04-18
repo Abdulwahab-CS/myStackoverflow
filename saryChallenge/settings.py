8b50eb834f67fe954030bdb747ed066f932613cc
Abdulwahab-CS <A.Alhendi.CS@gmail.com>
2021-04-18 11:26:23 +0300

    add general app to serve empyt route

--- saryChallenge/settings.py
+++ saryChallenge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#k9dy@l@fk_8!gb!2_9vnx%10ww3c#3%txph*0flt021&ss@3o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Mystackoverflow-env.eba-zqtcqeek.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mystackoverflow-dev.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # custom apps
+    'general',
     'questions',
     'answers',
     'comments',
