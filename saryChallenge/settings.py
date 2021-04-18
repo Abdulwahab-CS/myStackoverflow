d4063a089a8d07449cf051bb58d4c7c7f802178a
Abdulwahab-CS <A.Alhendi.CS@gmail.com>
2021-04-18 11:07:05 +0300

    solve small issue

--- saryChallenge/settings.py
+++ saryChallenge/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'saryChallenge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
