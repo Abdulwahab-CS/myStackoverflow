9f171918517e269c1cf009e706381a034ecfe751
Abdulwahab-CS <A.Alhendi.CS@gmail.com>
2021-04-18 18:31:46 +0300

    update welcoming index route text

--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Abdulwahab's stackoverflow API is working ...")
+    return HttpResponse('''
+        <div>
+            <h1 style="color:black;text-align:center;margin:30px;font-size:24px">
+                <span style="text-shadow:1px 1px lightgray">
+                    Abdulwahab's Stackoverflow API is working ...
+                </span>
+            </h1>
+        </div>
+    ''')
