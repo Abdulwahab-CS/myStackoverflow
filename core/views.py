a19bcacd0d3dec0bf1d78b41eecd142b08a13d79
Abdulwahab-CS <A.Alhendi.CS@gmail.com>
2021-04-18 18:54:44 +0300

    update index route message

--- core/views.py
+++ core/views.py
@@ -1,12 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('''
-        <div>
-            <h1 style="color:black;text-align:center;margin:30px;font-size:24px">
-                <span style="text-shadow:1px 1px lightgray">
-                    Abdulwahab's Stackoverflow API is working ...
-                </span>
-            </h1>
-        </div>
-    ''')
+    return HttpResponse("Abdulwahab's Stackoverflow API is working ...")
